3de556c423437fb47d8701258762a0927672015f
TheAwoo <stealthypaws@gmail.com>
2021-03-15 01:08:58 +0100

    Simple Economy.format implementation
    This change fixes interfacing with some economy-dependant plugins, for instance ShopChest. Related issue: EpicEricEE/ShopChest/issues/359

--- src/main/java/me/SuperRonanCraft/BetterEconomy/resources/economy/EconomyImplementer.java
+++ src/main/java/me/SuperRonanCraft/BetterEconomy/resources/economy/EconomyImplementer.java
@@ -41,7 +41,7 @@ public class EconomyImplementer implements Economy {
 
     @Override
     public String format(double v) {
-        return null;
+        return String.format("%,.0f", v);
     }
 
     @Override
